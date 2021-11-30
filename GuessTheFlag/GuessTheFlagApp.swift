8e2c0ef5b9c889adde4a80b02d5a4e8c372479bc
Dom Bryan <dombguitar@gmail.com>
2021-11-30 10:51:37 +0000

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Dom Bryan on 30/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
