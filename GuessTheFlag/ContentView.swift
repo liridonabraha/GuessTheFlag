790fa8c54ab5f7aad84310708dae3940dff8561e
Dom Bryan <dombguitar@gmail.com>
2021-12-21 16:38:41 +0000

    First commit

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,142 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var scoreMessage = ""
+    @State private var usersScore = 0
+    @State private var finalScore = false
+    @State private var rounds = 0
+    @State private var animationAmount = 0.0
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess the Flag")
+                    .titleStyle()
+                //                    .font(.largeTitle.bold())
+                //                    .foregroundColor(.white)
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                            withAnimation {
+                                animationAmount += 360
+                            }
+                        } label: {
+                            flagImagecountry(country: countries[number])
+                        }
+                        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(usersScore)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+            }
             .padding()
+        }
+        
+        
+        //
+        //-------------------------------------------------
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+            
+        } message: {
+            Text(scoreMessage)
+        }
+        //-------------------------------------------------
+        //
+        .alert("Finished", isPresented: $finalScore) {
+            Button("Reset", action: resetGame)
+        } message: {
+            Text("you scored \(usersScore) out of 8")
+            
+        }
+        
+    }
+    
+    func flagImagecountry(country: String) -> some View {
+        Image(country)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .shadow(radius: 5)
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            usersScore += 1
+            scoreMessage = "Your score is \(usersScore)"
+        } else {
+            scoreTitle = "Wrong"
+            scoreMessage = "Thats's the flag of \(countries[number])"
+        }
+        
+        showingScore = true
+        rounds += 1
+    }
+    
+    func askQuestion() {
+        if rounds == 8 {
+            finalScore = true
+        }
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func resetGame() {
+        usersScore = 0
+        rounds = 0
+    }
+
+}
+// Content view end
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
     }
 }
 
