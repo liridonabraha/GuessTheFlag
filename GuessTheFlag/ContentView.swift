9d0cf11a5e37125efce4f2bb4feb1be552616024
Liridona <donabraha94@gmail.com>
2022-05-08 18:01:18 +0100

    This is my flag app

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -14,16 +14,30 @@ struct ContentView: View {
     @State private var usersScore = 0
     @State private var finalScore = false
     @State private var rounds = 0
-    @State private var animationAmount = 0.0
+    @State private var selectedFlag = -1
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     var body: some View {
         ZStack {
             RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+                .init(color: Color(red: 0.2, green: 0.4, blue: 0.30), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.50, blue: 0.30), location: 0.3),
             ], center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
             
@@ -32,8 +46,8 @@ struct ContentView: View {
                 
                 Text("Guess the Flag")
                     .titleStyle()
-                //                    .font(.largeTitle.bold())
-                //                    .foregroundColor(.white)
+                    //.font(.largeTitle.bold())
+                    //.foregroundColor(.white)
                 
                 VStack(spacing: 15) {
                     VStack {
@@ -48,13 +62,14 @@ struct ContentView: View {
                     ForEach(0..<3) { number in
                         Button {
                             flagTapped(number)
-                            withAnimation {
-                                animationAmount += 360
-                            }
                         } label: {
                             flagImagecountry(country: countries[number])
                         }
-                        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+                        .rotation3DEffect(.degrees(selectedFlag == number ? 360 : 0), axis: (x: 0, y: 1, z: 0))
+                        .opacity(selectedFlag == -1 || selectedFlag == number ? 1 : 0.25)
+                        .saturation(selectedFlag == -1 || selectedFlag == number ? 1 : 0)
+                        .animation(.default, value: selectedFlag)
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                     }
                     
                 }
@@ -104,6 +119,7 @@ struct ContentView: View {
     }
     
     func flagTapped(_ number: Int) {
+        selectedFlag = number
         if number == correctAnswer {
             scoreTitle = "Correct"
             usersScore += 1
@@ -123,6 +139,7 @@ struct ContentView: View {
         }
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        selectedFlag = -1
     }
     
     func resetGame() {
---
+++ README.md
@@ -0,0 +1,1 @@
+This is the README for GuessThe Flag app 
